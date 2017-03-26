18d38ffdd66f1d059c5d7a99545f877a1f7473f9
sharath <sharath.savasere@gmail.com>
2017-03-26 11:32:32 +0530

    gesture test

--- untitled.js
+++ untitled.js
@@ -3,6 +3,35 @@ var scl = 20;
 
 var food;
 
+var el;
+var hammer;
+var swipeCount = 0;
+
+QUnit.module('Swipe Gesture', {
+    beforeEach: function() {
+        el = utils.createHitArea();
+        hammer = new Hammer(el, { recognizers: [] });
+        swipeCount = 0;
+      },
+    afterEach: function() {
+        hammer.destroy();
+      }
+  });
+
+QUnit.test('swipe can be recognized', function(assert) {
+    assert.expect(1);
+    var done = assert.async();
+    var swipe = new Hammer.Swipe({ threshold: 1 });
+    hammer.add(swipe);
+    hammer.on('swipe', function() {
+        assert.ok(true);
+        done();
+      });
+    Simulator.gestures.swipe(el);
+  });
+
+
+
 function setup() {
   createCanvas(600, 600);
   s = new Snake();
